9c497a9fecff9cba9d2791dc619d30d41efaa061
Chichi <chichii@vandenrobotics.com>
2021-09-21 16:49:18 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ app/src/main/java/com/example/projectone/activities/CharlotteAcitivity.java
@@ -0,0 +1,10 @@
+package com.example.projectone.activities;
+
+public class CharlotteAcitivity {
+
+
+    private EditText mentalHealth;
+    private EditText sleepQuestion;
+    private RadioButton oneTwoSleepQuestion;
+    private RadionButton threeFourSleepQuestion;
+}
