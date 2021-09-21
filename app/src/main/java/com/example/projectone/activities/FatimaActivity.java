c6b2ea1b9d9820642a102615cf97ed69bb062a1f
Chichi <Chichii@vandenrobotics.com>
2021-09-21 16:46:18 -0700

    organized strings

--- app/src/main/java/com/example/projectone/activities/FatimaActivity.java
+++ app/src/main/java/com/example/projectone/activities/FatimaActivity.java
@@ -1,4 +1,10 @@
 package com.example.projectone.activities;
 
 public class FatimaActivity {
+
+
+    private RadioButton eatNo;
+    private RadioButton eatYes;
+    private RadioButton eatNope;
+    private RadioButton
 }
