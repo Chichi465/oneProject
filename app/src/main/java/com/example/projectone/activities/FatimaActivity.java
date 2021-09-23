53587001875c05130cafaf05f366ffa19392c8b8
Chichi <Chichii@vandenrobotics.com>
2021-09-23 16:14:15 -0700

    Spinner changed cause chichi is confused
    :,)

--- app/src/main/java/com/example/projectone/activities/FatimaActivity.java
+++ app/src/main/java/com/example/projectone/activities/FatimaActivity.java
@@ -3,8 +3,47 @@ package com.example.projectone.activities;
 public class FatimaActivity {
 
 
-    private RadioButton eatNo;
-    private RadioButton eatYes;
-    private RadioButton eatNope;
-    private RadioButton
+    public static final String KEY_EatTodayQuestion = "EatTodayQuestion";
+    public static final String KEY_WaterTodayQuestion = "WaterTodayQuestion";
+    public static final String KEY_FoodGoodQuestion = "FoodGoodQuestion";
+    public static final String KEY_MealsTodayQuestion = "MealsTodayQuestion";
+    public static final String KEY_MeatTypeQuestion = "MeatTypeQuestion";
+    public static final String KEY_EatNo = "EatNo";
+    public static final String KEY_EatYes = "EatYes";
+    public static final String KEY_FoodMaybe = "FoodMaybe";
+    public static final String KEY_FoodNo = "FoodNo";
+    public static final String KEY_FoodYes = "FoodYes";
+    public static final String KEY_MealTwo = "MealTwo";
+    public static final String KEY_MealThree = "MealThree";
+    public static final String KEY_MealOne = "MealOne";
+    public static final String KEY_MealBlue = "MealBlue";
+    public static final String KEY_MealMore = "MealMore";
+    public static final String KEY_DrinkNo = "DrinkNo";
+    public static final String KEY_DrinkYes = "DrinkYes";
+    public static final String KEY_MeatBeef = "MeatBeef";
+    public static final String KEY_MeatSeafood = "MeatSeafood";
+    public static final String KEY_MeatChicken = "MeatChicken";
+    public static final String KEY_MeatPork = "MeatPork";
+
+    private String eatTodayQuestion;
+    private String waterTodayQuestion;
+    private String foodGoodQuestion;
+    private String mealsTodayQuestion;
+    private String meatTypeQuestion;
+    private int eatNo;
+    private int eatYes;
+    private int foodMaybe;
+    private int foodNo;
+    private int foodYes;
+    private int mealTwo;
+    private int mealThree;
+    private int mealOne;
+    private int mealBlue;
+    private int mealMore;
+    private int drinkNo;
+    private int drinkYes;
+    private int meatBeef;
+    private int meatSeafood;
+    private int meatChicken;
+    private int meatPork;
 }
