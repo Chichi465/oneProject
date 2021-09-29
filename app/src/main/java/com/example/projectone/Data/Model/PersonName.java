0ad51c2b0287d7a490cb0d70e1cbf9546ee856e4
Chichi <chichii@vandenrobotics.com>
2021-09-28 17:15:14 -0700

    change name to person name again whoops lol <3

--- app/src/main/java/com/example/projectone/Data/Model/PersonName.java
+++ app/src/main/java/com/example/projectone/Data/Model/PersonName.java
@@ -1,8 +1,8 @@
 package com.example.projectone.Data.Model;
 
 public class PersonName {
-    public static final String TABLE = "People";
-    public static final String KEY_PersonName = "People";
+    public static final String TABLE = "PersonName";
+    public static final String KEY_PersonName = "PersonName";
     private String personName;
     public PersonName(){
 
